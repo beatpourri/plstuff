4ffff14da34e80e318a4f4c507dba875d1286d25
beatpourri <beatpourri@users.noreply.github.com>
2019-02-15 16:03:20 +0100

    Update pattern.js

--- tools/generate-new-component/templates/pattern.js
+++ tools/generate-new-component/templates/pattern.js
@@ -1,3 +1,6 @@
-/**
- * <%= Name %>
- */
+// See components/_meta/_01-foot.twig for attachBehaviors
+ Drupal.behaviors.<%= name %> = {
+   attach: function (context, settings) {
+    'use strict';
+}
+ };
